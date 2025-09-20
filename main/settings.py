d904a41d5cc6e58a54264d9539b371c96614061a
Asraful Alam Shamrat <asrafulalamshamrat@Asrafuls-MacBook-Air.local>
2025-09-20 16:30:47 +0600

    Fix Some settings on the settings.py on the main project

--- main/settings.py
+++ main/settings.py
@@ -11,9 +11,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+
+# Add this for collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +32,7 @@ SECRET_KEY = 'django-insecure-s+eke*3t)xmafeo)k_dc#l2uikw$1yw4!mfs_&#!p*kg44c!3#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'new.dashboard.baitguests2.com']
 
 
 # Application definition
