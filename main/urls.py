801bc1a17a4001c3c8875d54d2df44db6f1a4867
Asraful Alam Shamrat <asrafulalamshamrat@Asrafuls-MacBook-Air.local>
2025-09-20 16:16:29 +0600

    Fix the api docs swagger url pattern on the main project

--- main/urls.py
+++ main/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path('camera/', include('camera.urls')),
     #path('office/', include('office.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 if settings.DEBUG:
