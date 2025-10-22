baea49da3f18fbdad6e499f61be9fdfe7a0df5fb
Asraful Alam Shamrat <asrafulalamshamrat@Asrafuls-MacBook-Air.local>
2025-10-22 18:40:54 +0600

    Add RFID to the admin panel

--- pilgrims/admin.py
+++ pilgrims/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Camera
+from .models import RFID
 
 @admin.register(Camera)
 class CameraAdmin(admin.ModelAdmin):
+    search_fields = ['sn', 'id']
+    list_display = ['id', 'office', 'sn']
+    
+
+@admin.register(RFID)
+class RFIDAdmin(admin.ModelAdmin):
     search_fields = ['sn', 'id']
     list_display = ['id', 'office', 'sn']
