d38c6c6eabfa21895b1012aba3f56aa8fa27638b
Asraful Alam Shamrat <asrafulalamshamrat@Asrafuls-MacBook-Air.local>
2025-11-08 01:37:45 +0600

    Adding time feature in the query for dashboard

--- office/views.py
+++ office/views.py
@@ -174,8 +174,8 @@ class DashboardIllegalPilgrims(APIView):
 
             filtered_entries = Pilgrim.objects.filter(
                 office=office,
-                #time_stamp__gte=start_date_time,
-                #time_stamp__lte=end_date_time
+                time_stamp__gte=start_date_time,
+                time_stamp__lte=end_date_time
             )
             total_detect_by_camera = filtered_entries.aggregate(
                 total=Sum('camera_count'))['total'] or 0
