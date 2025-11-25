443bd670ba1623ddca03ce1bec7df4ce7962e515
Asraful Alam Shamrat <asrafulalamshamrat@Asrafuls-MacBook-Air.local>
2025-11-25 20:35:25 +0600

    Trying with different table for both count. 2

--- office/urls.py
+++ office/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import OfficeApiView, DashboardIllegalPilgrims
 urlpatterns = [
     path('', OfficeApiView.as_view()),
-    path('dashboard/', DashboardIllegalPilgrims.as_view()),
+    #path('dashboard/', DashboardIllegalPilgrims.as_view()),
 ]
