30b0186e5ac42ab47257de1280281d3e28ee621b
Asraful Alam Shamrat <asrafulalamshamrat@Asrafuls-MacBook-Air.local>
2025-12-09 10:55:01 +0600

    Fix the celery task 1

--- main/settings.py
+++ main/settings.py
@@ -292,7 +292,7 @@ CELERY_TIMEZONE = 'Asia/Riyadh'
 
 CELERY_BEAT_SCHEDULE = {
     "merge-pilgrims-every-second": {
-        "task": "pilgrims.tasks.merge_pilgrims_every_second",
+        "task": "pilgrims.tasks.process_pilgrims_data",
         "schedule": 1.0,
     }
 }
