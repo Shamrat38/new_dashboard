e75113dd70120ed76ba54e143c762ba2aeb041e5
Asraful Alam Shamrat <asrafulalamshamrat@Asrafuls-MacBook-Air.local>
2025-12-21 00:13:49 +0600

    Time endpoint added 2

--- authentication/urls.py
+++ authentication/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path("users/", UserView.as_view()),
     path("users/<int:pk>/", UserView.as_view()),
-    path('token-refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('token-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('time/', ServerTime.as_view()),
 ]
