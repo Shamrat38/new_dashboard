405cc4af88772747c56cd5c48a360ae079ff31c3
Asraful Alam Shamrat <asrafulalamshamrat@Asrafuls-MacBook-Air.local>
2026-01-26 18:19:39 +0600

    Update model to store live images.

--- rfid_registry/admin.py
+++ rfid_registry/admin.py
@@ -4,8 +4,7 @@ from django.contrib import admin
 from .models import RFIDTag
 
 
-@admin.register(RFIDTag)
 class RFIDTagAdmin(admin.ModelAdmin):
-    list_display = ("epc_code", "name", "category", "is_active")
+    list_display = ("epc_code", "name", "category", "office", "is_active")
     search_fields = ("epc_code", "name")
-
+    list_filter = ("category", "office", "is_active")
--- rfid_registry/models.py
+++ rfid_registry/models.py
@@ -1,14 +1,29 @@
 from django.db import models
-
-from django.db import models
+from office.models import Office 
 
 
 class RFIDTag(models.Model):
     epc_code = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    category = models.CharField(max_length=100, blank=True, null=True)  # staff / pilgrim / cleaner / guard
+    category = models.CharField(max_length=100, blank=True, null=True)
     notes = models.TextField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
+
+    # ✅ NEW
+    office = models.ForeignKey(
+        Office,
+        on_delete=models.CASCADE,
+        related_name="rfid_tags",
+        null=True,
+        blank=True
+    )
+
+    # ✅ NEW
+    image = models.ImageField(
+        upload_to="rfid_tags/%Y/%m/%d/",
+        null=True,
+        blank=True
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
 
