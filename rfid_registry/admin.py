952dcc70db3669cd1915105312973cca2593c485
Asraful Alam Shamrat <asrafulalamshamrat@Asrafuls-MacBook-Air.local>
2026-01-26 18:31:28 +0600

    Update model to store live images.1

--- rfid_registry/admin.py
+++ rfid_registry/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-
-from django.contrib import admin
 from .models import RFIDTag
+from django.utils.html import format_html
 
-
+@admin.register(RFIDTag)
 class RFIDTagAdmin(admin.ModelAdmin):
-    list_display = ("epc_code", "name", "category", "office", "is_active")
+    list_display = ("epc_code", "name", "category", "office", "is_active", "image_preview")
     search_fields = ("epc_code", "name")
     list_filter = ("category", "office", "is_active")
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="60" height="60" />', obj.image.url)
+        return "No Image"
+
+    image_preview.short_description = "Image"
