2d5c85e9fff56e5e494b404df6c3bc3c4b6b2173
Asraful Alam Shamrat <asrafulalamshamrat@Asrafuls-MacBook-Air.local>
2026-02-16 15:29:26 +0600

    Celery tast off

--- main/celery.py
+++ main/celery.py
@@ -4,8 +4,8 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
-app = Celery('main')
+#app = Celery('main')
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+#app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks()
+#app.autodiscover_tasks()
